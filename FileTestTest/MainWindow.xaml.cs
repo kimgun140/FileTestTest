7a09b63f2cebbaf6551a91d701a081540bd43279
gkim <gkim@gemiso.com>
2025-02-17 18:33:11 +0900

    asdf

--- FileTestTest/MainWindow.xaml.cs
+++ FileTestTest/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Runtime.InteropServices;
+using System.Security.Policy;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,10 +12,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using static System.Net.Mime.MediaTypeNames;
+
 namespace FileTestTest
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+//hehe hehe
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -22,26 +25,29 @@ namespace FileTestTest
         [DllImport("mpr.dll")]
         // Windows API 함수 WNetAddConnection2를 호출하기 위한 선언입니다. 
         // Multiple Provider Router DLL 에서 제공하는 함수, smb 네트워크 드라이브 연결을 설정하는 역할을 한다. netresource 구조체를 전달하여 네트워크 경로를 지정하고, username과 password로 인증을 수행한다. 
-        // 접근권한 주는구나
+        // 스토리지 접근권한 주기
         private static extern int WNetAddConnection2(ref NETRESOURCE netResource, string password, string username, int flags);
 
         [DllImport("mpr.dll")]
         // smb 네트워크 드라이브 연결을 해재하는 기능을 한다.
         private static extern int WNetCancelConnection2(string name, int flags, bool force);
-        [StructLayout(LayoutKind.Sequential)]
+        [StructLayout(LayoutKind.Sequential)]// 메모리에 순차적으로 배치하다
         private struct NETRESOURCE
         // 네트워크 리소스 나타내는 구조체 
         {
             public int dwScope;
-            public int dwType;
+            public int dwType;//리소스 유형 1 이면 디스크 드라이브 
             public int dwDisplayType;
             public int dwUsage;
-            public string lpLocalName;
-            public string lpRemoteName;
+            public string lpLocalName;// 로컬드라이브 문자, 매핑할 경우 사용 
+            public string lpRemoteName;// 원격 경로
             public string lpComment;
             public string lpProvider;
         }
-
+        string uncPath = @"\\gms-mcc-nas01\AUDIO-FILE\test1\test121.txt";
+        //string content = "이것은 공유 스토리지에 저장되는 테스트 파일입니다.";
+        string username = "develop";
+        string password = "Akds0ft3!48";
         public MainWindow()
         {
             InitializeComponent();
@@ -49,11 +55,8 @@ namespace FileTestTest
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string uncPath = @"\\gms-mcc-nas01\AUDIO-FILE\test1\test121.txt";
-            //string content = "이것은 공유 스토리지에 저장되는 테스트 파일입니다.";
-            string username = "develop";
-            string password = "Akds0ft3!48";
 
+            //구조체에 연결에 필요한 정보가 있나
             NETRESOURCE netResource = new NETRESOURCE
             {
                 dwType = 1,
@@ -62,12 +65,13 @@ namespace FileTestTest
             // 구조체 초기화해 경로 설정 
             int result = WNetAddConnection2(ref netResource, password, username, 0);
 
-            if (result == 0)
+            if (result == 0)// 0이면 연결 
             {
 
                 try
                 {
-                    //File.WriteAllText
+                    File.WriteAllText(uncPath + "0217", "이것은 테스트파일입니다."); /// //////
+                    MessageBox.Show("테스트");
 
                     using (StreamWriter sw = new StreamWriter(uncPath, true)) // true: 기존 파일에 추가
                     {
@@ -80,12 +84,180 @@ namespace FileTestTest
                     MessageBox.Show(ex.Message);
                 }
                 WNetCancelConnection2(@"\\gms-mcc-nas01\AUDIO-FILE\test1", 0, true);
-                //연결해제
+                //연결해제 lpRemoteName 을 넣어주기  
+                //windows 네트워크 드라이브는 네트워크 공유 폴더 단위로 관리되므로, 연결을 해제할때 해당 공유폴더의 경로를 지정한다.
             }
             else
             {
+
                 MessageBox.Show(result.ToString());
             }
+        }
+
+
+        public void ChangingTest()
+        {
+            NETRESOURCE nETRESOURCE = new NETRESOURCE
+            {
+                lpRemoteName = @"\\gms-mcc-nas01\AUDIO-FILE\test1",
+                dwType = 1,
+
+
+            };
+            int result = WNetAddConnection2(ref nETRESOURCE, password, username, 0);
+            try
+            {
+                string appendText = "\n새로운 내용이 추가되었습니다.";
+                File.AppendAllText(uncPath, appendText);
+         
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message +$"{result}");
+            }
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ChangingTest();
+        }
+        public void AttibuteChangingtest()
+            // 파일속성을 읽기 전용으로 변경해서 수정이 안된다. 다른 메서드로 다시 쓰거나, 변경하려고하면 권한 에러? 변경 안됨 
+        {
+            NETRESOURCE nETRESOURCE = new NETRESOURCE
+            {
+                lpRemoteName = @"\\gms-mcc-nas01\AUDIO-FILE\test1",
+                dwType = 1,
+            };
+            int result = WNetAddConnection2( ref nETRESOURCE, password, username, 0);
+
+            try
+            {
+                File.SetAttributes(uncPath, FileAttributes.Normal);
+                MessageBox.Show("파일이 읽기 전용으로 설정되었습니다.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{result},{ex.Message}");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            AttibuteChangingtest();
+        }
+
+       public void AuthTest()
+            // 파일 권한 및 인증  이거 뭐지 계정의 권한이 없으면 공유 스토리지에 접근 못할
+        {
+            var unc = @"\\gms-mcc-nas01\AUDIO-FILE\test1";
+            NETRESOURCE nETRESOURCE = new NETRESOURCE
+            {
+                lpRemoteName = unc,
+                dwType = 1,
+
+            };
+            int result = WNetAddConnection2(ref nETRESOURCE, password, username, 0);
+            var fileInfo = new FileInfo(unc);
+            MessageBox.Show(fileInfo.FullName);
+            var acl = fileInfo.GetAccessControl();
+            MessageBox.Show(acl.ToString());
+           //MessageBox.Show( acl.GetOwner(typeof(System.Security.Principal.NTAccount)).ToString());
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            AuthTest();
+
+        }
+        public void retryTest()
+        {
+            var unc = @"\\gms-mcc-nas01\AUDIO-FILE\test1";
+            NETRESOURCE nETRESOURCE = new NETRESOURCE
+            {
+                lpRemoteName = unc,
+                dwType = 1,
+
+            };
+            int result = WNetAddConnection2(ref nETRESOURCE, password, username, 0);
+            int retryCount = 3;
+            unc += $"{ DateTime.Now:yyyyMMddHHmmss}.txt";
+            while (retryCount > 0)
+            {
+                try
+                {
+                    File.WriteAllText(uncPath, "데이터 저장");
+                    Console.WriteLine("파일 저장 성공!");
+                    break;
+                }
+                catch (IOException)
+                {
+                    retryCount--;
+                    Console.WriteLine("파일 접근 실패, 재시도...");
+                    System.Threading.Thread.Sleep(1000); // 1초 후 재시도
+                }
+            }
+        }
+       public void fsWrite()
+        {
+            var streamclasss = new streamclass();
+            string uncPathtest = @"\\gms-mcc-nas01\AUDIO-FILE\test1\testFilestream.txt";
+            streamclasss.WriteFile(uncPathtest);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            fsWrite();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            var streamclasss = new streamclass();
+            string uncPathtest = @"\\gms-mcc-nas01\AUDIO-FILE\test1\TestBIN.bin";
+            streamclasss.BinaryWriteFile(uncPathtest);
+            MessageBox.Show("바이너리write");
+            streamclasss. BinaryReadFile( uncPathtest );
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            var streamclasss = new streamclass();
+            string uncPathtest = @"\\gms-mcc-nas01\AUDIO-FILE\test1\bytebintest.bin";
+            streamclasss.ByteWriteReader( uncPathtest );
+            string uncPathtest2 = @"\\gms-mcc-nas01\AUDIO-FILE\test1\";
+            streamclasss.filecopytest(uncPathtest,uncPathtest2);
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            var streamclasss  = new streamclass();
+            string uncPathtest = "C:\\Users\\kimgu\\OneDrive\\바탕 화면\\20241204000000_녹음0.wav";
+            string uncPathtest2 = @"\\gms-mcc-nas01\AUDIO-FILE\test1\AudioTest2.mp3";
+            streamclasss.doublestream(uncPathtest,uncPathtest2);
+
+
+
+        }
+
+        private async void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            //copyAudioTest
+            var streamclasss = new streamclass();
+            string uncPathtest = "C:\\Users\\kimgu\\OneDrive\\바탕 화면\\20241204000000_녹음0.wav";
+            string uncPathtest2 = @"\\gms-mcc-nas01\AUDIO-FILE\test1\AudioTestCopy123.mp3";
+
+            streamclasss.CopyTest(uncPathtest, uncPathtest2);
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            WNetCancelConnection2(@"\\gms-mcc-nas01\AUDIO-FILE\test1", 0, true);
         }
     }
 }
---
+++ FileTestTest/streamclass.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileTestTest
+{
+    public class streamclass
+    {
+
+
+        public void WriteFile(string filePath)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine("테스트 파일입니다.");
+                    sw.WriteLine("FileStream 클래스로 작성");
+                }
+                MessageBox.Show("작업 완료");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        public void BinaryWriteFile(string filePath)
+        {
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+            using (BinaryWriter bw = new BinaryWriter(fs))
+            // 데이터 기록은 한줄씩 되는게 아니라 순서대로 연속적으로 기록이 된다.
+            // 
+            {
+                //bw.Write(filePath);
+                bw.Write(42);
+                bw.Write(3.14);
+                bw.Write(true);
+                bw.Write("Hello");
+                bw.Flush();
+
+
+            }
+
+
+        }
+        public void BinaryReadFile(string Filepath)
+        {
+            using (FileStream fs = new FileStream(Filepath, FileMode.Open, FileAccess.Read))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                int number = br.ReadInt32();
+                double pi = br.ReadDouble();
+                bool flag = br.ReadBoolean();
+                string text = br.ReadString();
+                // int를 읽으라고해서 인트의 길이만큼 읽어오고 거기까지 읽은 표시를 남기고 그다음에 읽어도록 되어있는거네 뭔가 대단한게 있는게 아니라 
+
+                MessageBox.Show($"{number},{pi},{flag}, {text}");
+            }
+        }
+
+        public void ByteWriteReader(string filepath)
+        {
+            byte[] datatowrite = new byte[1024];//초기화
+            new Random().NextBytes(datatowrite);// 이거 뭐지 무작위 숫자로 채우는거네 
+
+            using (FileStream fs = new FileStream(filepath, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(datatowrite, 0, datatowrite.Length);// 파일포인터가 이동함 
+                //fs.Write(datatowrite);// 뭘까 얘는 좋은거라고 하는데 이해가 안되넹?
+                MessageBox.Show("작성완료");
+            }
+
+            byte[] dataRead = new byte[1024];
+
+            using (FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                fs.Read(dataRead, 0, dataRead.Length);
+                MessageBox.Show($"읽은 데이터{CompareArrays(datatowrite, dataRead)}");
+            }
+
+        }
+        public bool CompareArrays(byte[] a, byte[] b)
+        {
+            if (a.Length != b.Length) return false;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i]) return false;
+
+            }
+            return true;
+        }
+
+
+        public void filecopytest(string sourcepath, string sourthpath2)
+        {
+            string destinationPath = sourthpath2 + "testdestination.bin";// 경로를 깡으로 적으니까 공유스토리지에 안되었네 
+            File.Copy(sourcepath, destinationPath, true); // true는 덮어쓰기 허용 file.copy는 파일 전체를한번에 복사해버림 그리고 파일 내에서 파일포인터 통제도 되안됨 그래서 share access도 종제안됨 
+            //copy는 fileAccess FileShare같은 옵션이 없네 그래서 다른 프로세스스에서 작업중일 경우에는 잠겨버리네 접근을 못하게 해버림 그래서 파일이 열려있는 상태인지 잠겨 있는지도 확인해야겠네 
+            byte[] dataRead = new byte[1024];//버퍼크기 
+            int bytesRead;// read가 반환하는 값 0 나오면 파일 끝까지 읽은거임 
+            // 
+            using (FileStream fs = new FileStream(destinationPath, FileMode.Open, FileAccess.Read)) //읽기 
+            {
+                while ((bytesRead = fs.Read(dataRead, 0, destinationPath.Length)) > 0) //파일포인터? 그게 알아서 위치이동하네 
+                {
+
+                    MessageBox.Show($"읽은 데이터{dataRead}");
+                }
+            }
+        }
+
+        public async void doublestream(string sourcePath, string destinationPath)
+        //파일 읽는 스트림 , 쓰는 스트림 2개 스트림 2개를 열어서 하나는 버퍼크기만큼 읽어오고 다른 하나는 쓰는거구나 
+        {
+            // 스트림 2개 사용해서 
+            int retryCount = 3;// 재시도 
+
+            while (retryCount > 0)// 여기서 재시도해
+            {
+                using (FileStream sourceStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))// 읽기
+                using (FileStream destinationStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write)) // 쓰기 
+                {
+                    try
+                    {
+                        byte[] buffer = new byte[4096]; // 4KB 버퍼
+                        int bytesRead;
+
+                        while ((bytesRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await destinationStream.WriteAsync(buffer, 0, bytesRead);
+                        }
+                        return;// 완료되면 리떤
+                    }
+                    catch (IOException ex)
+                    {
+                        retryCount--;
+                        if (retryCount > 0)
+                        {
+                            await Task.Delay(100);// 잠시 대기 
+                        }
+                        else
+                        {
+                            MessageBox.Show($"실패!!{ex.Message}", "오류", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+        }
+        public void CopyTest(string sourcePath, string destinationPath)
+            // copy로 오디오파일 복사하기 
+        {
+            File.Copy(sourcePath, destinationPath, true);
+        }
+    }
+}
